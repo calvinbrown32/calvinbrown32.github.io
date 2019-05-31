ff552cd40f7bd976a69b08d87d265a46aac804bd
calvinbrown32 <35974887+calvinbrown32@users.noreply.github.com>
2019-05-31 14:32:07 -0400

    Create test.js

---
+++ JWAFiles/test.js
@@ -0,0 +1,62 @@
+var test = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "popupContent": "18th & California Light Rail Stop"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -104.98999178409576,
+          39.74683938093904
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "popupContent": "20th & Welton Light Rail Stop"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -104.98689115047453,
+          39.747924136466565
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              7644117.326660156,
+              45.53737715921508
+            ],
+            [
+              7644117.313699722,
+              45.52048102077873
+            ],
+            [
+              7644117.347431182,
+              45.51536908938486
+            ],
+            [
+              7644117.346744538,
+              45.52643433190143
+            ],
+            [
+              7644117.326660156,
+              45.53737715921508
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
